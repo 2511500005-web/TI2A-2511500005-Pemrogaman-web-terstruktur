59ada6c97504aa59809a68028be82eb6425f6527
2511500005-web <2511500005@mahasiswa.atmaluhur.ac.id>
2026-03-10 21:30:01 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 <?php
 session_start();
 
-// --- LOGOUT ---
+
 if (isset($_GET['action']) && $_GET['action'] === 'logout') {
     session_destroy();
     header('Location: index.php');
     exit;
 }
 
-// --- LOGIN PROSES (selalu berhasil) ---
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['login'])) {
     $_SESSION['username'] = $_POST['username'] ?: 'Guest';
     header('Location: index.php');
     exit;
 }
 
-// --- CEK LOGIN ---
+
 $logged_in = isset($_SESSION['username']);
 ?>
 <!DOCTYPE html>
@@ -29,7 +29,7 @@ $logged_in = isset($_SESSION['username']);
 <body class="hold-transition <?php echo $logged_in ? 'sidebar-mini' : 'login-page'; ?>">
 
 <?php if (!$logged_in): ?>
-<!-- FORM LOGIN -->
+
 <div class="login-box">
   <div class="login-logo"><b>Admin</b>LTE</div>
   <div class="card">
@@ -55,16 +55,16 @@ $logged_in = isset($_SESSION['username']);
 </div>
 
 <?php else: ?>
-<!-- DASHBOARD -->
+
 <div class="wrapper">
-  <!-- Navbar -->
+  
   <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <ul class="navbar-nav">
+   
       <li class="nav-item d-none d-sm-inline-block"><a href="index.php" class="nav-link active">Home</a></li>
     </ul>
   </nav>
 
-  <!-- Sidebar -->
+ 
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <a href="index.php" class="brand-link"><span class="brand-text font-weight-light">AdminLTE 3</span></a>
     <div class="sidebar">
@@ -73,7 +73,7 @@ $logged_in = isset($_SESSION['username']);
       </div>
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu">
-          <!-- Master -->
+          
           <li class="nav-item has-treeview">
             <a href="#" class="nav-link"><i class="nav-icon fas fa-book"></i><p>Master<i class="right fas fa-angle-left"></i></p></a>
             <ul class="nav nav-treeview">
@@ -83,14 +83,14 @@ $logged_in = isset($_SESSION['username']);
               <li class="nav-item"><a href="#" class="nav-link"><p>Kelas</p></a></li>
             </ul>
           </li>
-          <!-- Transaksi -->
+          
           <li class="nav-item has-treeview">
             <a href="#" class="nav-link"><i class="nav-icon fas fa-calendar"></i><p>Transaksi<i class="right fas fa-angle-left"></i></p></a>
             <ul class="nav nav-treeview">
               <li class="nav-item"><a href="#" class="nav-link"><p>Jadwal</p></a></li>
             </ul>
           </li>
-          <!-- Akses Guru -->
+          
           <li class="nav-item has-treeview">
             <a href="#" class="nav-link"><i class="nav-icon fas fa-chalkboard-teacher"></i><p>Akses Guru<i class="right fas fa-angle-left"></i></p></a>
             <ul class="nav nav-treeview">
@@ -99,7 +99,7 @@ $logged_in = isset($_SESSION['username']);
               <li class="nav-item"><a href="#" class="nav-link"><p>Jadwal</p></a></li>
             </ul>
           </li>
-          <!-- Akses Siswa -->
+          
           <li class="nav-item has-treeview">
             <a href="#" class="nav-link"><i class="nav-icon fas fa-user-graduate"></i><p>Akses Siswa<i class="right fas fa-angle-left"></i></p></a>
             <ul class="nav nav-treeview">
@@ -107,21 +107,21 @@ $logged_in = isset($_SESSION['username']);
               <li class="nav-item"><a href="#" class="nav-link"><p>Jadwal</p></a></li>
             </ul>
           </li>
-          <!-- Database Export -->
+          
           <li class="nav-item">
             <a href="http://localhost/phpmyadmin/index.php?route=/export" target="_blank" class="nav-link">
               <i class="nav-icon fas fa-database"></i>
               <p>Database Export</p>
             </a>
           </li>
-          <!-- Logout -->
+          
           <li class="nav-item"><a href="index.php?action=logout" class="nav-link"><i class="nav-icon fas fa-sign-out-alt"></i><p>Logout</p></a></li>
         </ul>
       </nav>
     </div>
   </aside>
 
-  <!-- Content -->
+ 
   <div class="content-wrapper">
     <section class="content-header"><div class="container-fluid"><h1>Starter Page</h1></div></section>
     <section class="content">
@@ -138,7 +138,7 @@ $logged_in = isset($_SESSION['username']);
 </div>
 <?php endif; ?>
 
-<!-- Scripts -->
+
 <script src="plugins/jquery/jquery.min.js"></script>
 <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
 <script src="dist/js/adminlte.min.js"></script>
