35d77aaeb9f3af5e5c9342edf0c89f7297fb6923
Melvyn Mahinda Hadi <2511500005@mahasiswa.atmaluhur.ac.id>
2026-03-31 18:41:12 +0700

    save

--- Pertemuan-3/index.php
+++ Pertemuan-3/index.php
@@ -3,33 +3,30 @@ include "config.php";
 
 if(isset($_SESSION['login'])){
     header("Location: dashboard.php");
-    exit;
 }
 
 if(isset($_POST['login'])){
     $_SESSION['login']=true;
-
-    if(strtolower($_POST['username'])=="guru"){
-        $_SESSION['role']="guru";
-    } else {
-        $_SESSION['role']="siswa";
-    }
-
+    $_SESSION['role'] = (strtolower($_POST['username'])=="guru") ? "guru" : "siswa";
     header("Location: dashboard.php");
-    exit;
 }
 ?>
+
 <html>
-<head><link rel="stylesheet" href="assets/style.css"></head>
+<head>
+<link rel="stylesheet" href="assets/style.css">
+</head>
 <body class="login-body">
+
 <div class="login-box">
-<h2>Login (Bebas)</h2>
+<h2>Login Bebas</h2>
 <form method="POST">
 <input name="username" placeholder="Username"><br>
 <input type="password" name="password" placeholder="Password"><br>
 <button name="login">Login</button>
 </form>
-<p>Ketik "guru" untuk akses guru, selain itu siswa</p>
+<p>Ketik "guru" untuk akses guru</p>
 </div>
+
 </body>
 </html>
--- Pertemuan-3/kelas.php
+++ Pertemuan-3/kelas.php
@@ -1,29 +1,40 @@
 <?php include "config.php";
 if($_SESSION['role']!="guru") header("Location:dashboard.php"); ?>
-<html><head><link rel="stylesheet" href="assets/style.css"></head>
+
+<html>
+<head><link rel="stylesheet" href="assets/style.css"></head>
 <body>
+
 <?php include "partials/header.php"; ?>
 <?php include "partials/sidebar.php"; ?>
+
 <div class="content">
-<h2>Kelas - Absensi</h2>
+<h2>Absensi Mahasiswa</h2>
+
 <table>
 <tr><th>No</th><th>NIM</th><th>Status</th></tr>
+
 <?php for($i=1;$i<=31;$i++):
 $nim=str_pad($i,3,"0",STR_PAD_LEFT); ?>
+
 <tr>
 <td><?= $i ?></td>
 <td><?= $nim ?></td>
 <td>
 <select onchange="ubahWarna(this)">
-<option value=""></option>
+<option></option>
 <option value="hadir">Hadir</option>
 <option value="izin">Izin</option>
 <option value="alpa">Alpa</option>
 </select>
 </td>
 </tr>
+
 <?php endfor; ?>
+
 </table>
 </div>
+
 <?php include "partials/footer.php"; ?>
-</body></html>
+</body>
+</html>
