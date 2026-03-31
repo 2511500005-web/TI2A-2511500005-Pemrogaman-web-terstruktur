7b47d9395522c0daf9cc29988a5b936ced02f12f
Melvyn Mahinda Hadi <2511500005@mahasiswa.atmaluhur.ac.id>
2026-03-31 16:14:48 +0700

    Menghapus file Pert.001 dan mwmodifikasi file

---
+++ Pertemuan-1/dashboard.php
@@ -0,0 +1,2 @@
+<h1>Dashboard</h1>
+<p>Selamat Datang di Situs aku</p>
---
+++ Pertemuan-1/jadwal.php
@@ -0,0 +1,9 @@
+<h4>Jadwal Kuliah</h4>
+<table>
+  <tr><th>No</th><th>Kode</th><th>Mata Kuliah</th><th>SKS</th><th>Kelas</th><th>Hari, Jam</th><th>Ruang</th><th>Dosen</th></tr>
+  <tr><td>1</td><td>IT202</td><td>Kalkulus 2</td><td>3</td><td>TI2A</td><td>Senin, 08:00–09:30</td><td>2.2.4</td><td>R. Burham</td></tr>
+  <tr><td>2</td><td>IF901</td><td>Algoritma dan Struktur Data</td><td>4</td><td>TI2A</td><td>Senin, 13:00–16:30</td><td>LAB 3</td><td>Elza Budi</td></tr>
+  <tr><td>3</td><td>IT311</td><td>Pemrograman Web Terstruktur</td><td>3</td><td>TI2A</td><td>Selasa, 13:00–15:30</td><td>LAB 3</td><td>Delpiah W.</td></tr>
+  <tr><td>4</td><td>IT305</td><td>Sistem Manajemen Basis Data</td><td>3</td><td>TI2A</td><td>Rabu, 08:00–09:30</td><td>2.2.4</td><td>Metde Suci</td></tr>
+</table>
+<p><strong>Jumlah SKS: 20</strong></p>
