ff1b06d2bd404775b645b56f0cd78d57131a7c10
2511500005-web <2511500005@mahasiswa.atmaluhur.ac.id>
2026-05-26 16:06:09 +0700

    Add files via upload

--- Pertemuan-15/sidebar.php
+++ Pertemuan-15/sidebar.php
@@ -1,8 +1,28 @@
- <div class="sidebar">
+- <div class="sidebar">
 
 <h3>Akses</h3>
 
-<?php if($_SESSION['role']=="guru"): ?>
+<?php if($_SESSION['role']=="admin"): ?>
+
+<b>Menu Admin</b>
+
+<a href="dashboard.php">Dashboard</a>
+
+<a href="jadwal.php">Jadwal</a>
+
+<a href="detail_jadwal.php">
+Detail Jadwal
+</a>
+
+<a href="cetak_jadwal.php">
+Cetak Jadwal
+</a>
+
+<a href="ekstraNimAnda.php">
+Ekstrakurikuler
+</a>
+
+<?php elseif($_SESSION['role']=="guru"): ?>
 
 <b>Menu Guru</b>
 
@@ -12,9 +32,17 @@
 
 <a href="jadwal.php">Jadwal</a>
 
-<a href="detail_jadwal.php">Detail Jadwal</a>
+<a href="lihat_jadwal.php">
+Jadwal Saya
+</a>
 
-<a href="ekstraNimAnda.php">Ekstrakurikuler</a>
+<a href="detail_jadwal.php">
+Detail Jadwal
+</a>
+
+<a href="ekstraNimAnda.php">
+Ekstrakurikuler
+</a>
 
 <?php else: ?>
 
@@ -22,16 +50,24 @@
 
 <a href="dashboard.php">Dashboard</a>
 
-<a href="jadwal.php">Jadwal</a>
+<a href="lihat_jadwal.php">
+Jadwal Kelas
+</a>
 
-<a href="detail_jadwal.php">Detail Jadwal</a>
+<a href="detail_jadwal.php">
+Detail Jadwal
+</a>
 
-<a href="ekstraNimAnda.php">Ekstrakurikuler</a>
+<a href="ekstraNimAnda.php">
+Ekstrakurikuler
+</a>
 
 <?php endif; ?>
 
 <hr>
 
-<a href="logout.php">Logout</a>
+<a href="logout.php">
+Logout
+</a>
 
 </div>
